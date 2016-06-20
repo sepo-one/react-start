2a90cf3db7b6425efbc47593d02a9ed27408d528
sepo-one <sepo@b-one.net>
2016-06-20 12:46:30 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p>Hello from React! Now Reload works</p>;
+    return <p>React start blueprint</p>;
   }
 }
 
